e26482e808194d22e258c2be2c5140ec5b6b083d
ela-hidri <elahidri.eh@gmail.com>
2023-03-16 13:54:46 -0700

    creating my first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * puts - prints "Programming is like building a multilingual puzzle" without using printf
+ *
+ */
+void puts(){
+putchar("Programming is like building a multilingual puzzle\n");
+}
+
+
+int main(void)
+{
+	puts();
+    return (0);
+}
