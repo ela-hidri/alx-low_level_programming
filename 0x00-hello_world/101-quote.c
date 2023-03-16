348d19812b555aefdfce217c64aa0ef58b954444
ela-hidri <elahidri.eh@gmail.com>
2023-03-16 15:02:50 -0700

    print withour puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-while (*s) putchar(*s++);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 return (0);
 }
