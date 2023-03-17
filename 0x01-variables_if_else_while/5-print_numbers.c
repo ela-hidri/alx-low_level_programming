7069733deb4e86c8a0c89a85e8af681562cf9722
ela-hidri <elahidri.eh@gmail.com>
2023-03-17 10:54:58 -0700

    creating pritn numbers file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * Rerurn : Always 0 (Succes)
@@ -8,7 +7,7 @@
 int main(void)
 {
 int i = 0;
-for (i = 0;i < 10;i++ )
+for (i = 0; i < 10; i++)
 {
 printf("%d", i);
 }
