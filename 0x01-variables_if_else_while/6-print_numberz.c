994d7e4e8e3a91ac9747d5866b09e7714d37c305
ela-hidri <elahidri.eh@gmail.com>
2023-03-17 11:07:11 -0700

    create print numbers with putchar file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Rerurn : Always 0 (Succes)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 0;i < 10;i++ )
+{
+putchar(48+i);
+}
+putchar('\n');
+return (0);
+}
