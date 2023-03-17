eee521cc98556c435dd45d6a27451588e60cfe62
ela-hidri <elahidri.eh@gmail.com>
2023-03-17 11:15:28 -0700

    Fixxing betty style

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i = 0;
-for (i = 0 ;i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 putchar(48 + i);
 }
