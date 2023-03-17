812b820d40be6d108a28580e42e15572b81ec324
ela-hidri <elahidri.eh@gmail.com>
2023-03-17 12:33:43 -0700

    creating thrd advanced combo file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+int j = 0;
+int k = 0;
+int l = 0;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 9; j++)
+{
+for (k = 0; k < 10; k++)
+{
+for (l = 0; l < 10; l++)
+{
+if ((i > j && k < l && i < k && j < l) && (i != k && i!=l && j!=k && j!=l))
+{
+putchar(48 + i);
+putchar(48 + j);
+putchar(' ');
+putchar(48 + k);
+putchar(48 + l);
+if (i != 9 || j != 8 || k != 9 || l != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
