5e513e9740a7de87ba960d032536ee040c251fcf
ela-hidri <elahidri.eh@gmail.com>
2023-03-17 14:03:42 -0700

    creating double combo file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,21 +9,21 @@ int main(void)
 int i = 0;
 int j = 0;
 for(i = 0; i < 100 ; i++){
-for(j = 0; j < 100 ; j++){
-if ((i+i%10) != (j+j%10) && i<j )
-{
-putchar((i/10)+'0');
-putchar((i%10)+'0');
+for(j = i+1; j < 100 ; j++){
+
+
+putchar((i/10)+48);
+putchar((i%10)+48);
 putchar(' ');
-putchar((j/10)+'0');
-putchar((j%10)+'0'); 
+putchar((j/10)+48);
+putchar((j%10)+48); 
 if(i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
+
 }
     putchar('\n');
 
