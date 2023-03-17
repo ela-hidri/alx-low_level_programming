65789eeb72d1ad2b1bc91a7ba409761a6517d097
ela-hidri <elahidri.eh@gmail.com>
2023-03-17 11:23:52 -0700

    creating reverse alphabet file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = 122;
+while (c > 96)
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
