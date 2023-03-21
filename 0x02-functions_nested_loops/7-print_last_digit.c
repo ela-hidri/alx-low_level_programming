43dd0e9f5e6a409b1c8e111eb044ed42880fea6b
ela-hidri <elahidri.eh@gmail.com>
2023-03-21 14:03:18 -0700

    adding print last digit file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,10 @@
  */
 int print_last_digit(int i)
 {
-if (i <0 )
-{
-_putchar(((i * (-1)) % 10) + '0');
-return ((i * (-1)) % 10);
-}
-_putchar((i % 10) + '0');
-return (i % 10);
+int last_digit = 0;
+last_digit = i % 10;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + '0');
+return (last_digit);
 }
