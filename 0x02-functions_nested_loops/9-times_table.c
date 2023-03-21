5a1a1a7301d6d09e4b69f6647f729bdcb3573810
ela-hidri <elahidri.eh@gmail.com>
2023-03-21 14:44:01 -0700

    adding 9-times _table file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,22 @@ void times_table(void)
 {
 int i = 0;
 int j = 0;
+int rst = 0;
 for (i = 0; i < 10; i++)
 {
 	for (j = 0; j < 10; j++)
 	{
-	_putchar((i * j) + '0');
-	_putchar(',');
-	_putchar(' ');
+	rst = i * j;
+	if (rst < 10)
+		_putchar(' ');
+	else 
+		_putchar(rst / 10 + '0');
+	_putchar(rst % 10 + '0');
+	if(i !=9 || j != 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
 	}
 _putchar('\n');
 }
