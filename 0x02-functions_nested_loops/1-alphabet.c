34f3d0e333036516b38b416015c65ec3ac1aa719
ela-hidri <elahidri.eh@gmail.com>
2023-03-21 12:45:48 -0700

    adding 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include 'main.h'
+
+/**
+ *  print_alphabet - writs all the alphabet
+ *
+ * Return:  success always.
+ */
+
+void print_alphabet()
+{
+int c = 97;
+while (c < 123)
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
