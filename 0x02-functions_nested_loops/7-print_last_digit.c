a49c5293f8be77e7238ad4603383192c38591016
ela-hidri <elahidri.eh@gmail.com>
2023-03-21 13:53:47 -0700

    adding print last digit file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@ int print_last_digit(int i)
 {
 if (i <0 )
 {
-_putchar((i * (-1)) % 10);
+_putchar(((i * (-1)) % 10) + '0');
 return ((i * (-1)) % 10);
 }
-_putchar(i % 10);
+_putchar((i % 10) + '0');
 return (i % 10);
 }
