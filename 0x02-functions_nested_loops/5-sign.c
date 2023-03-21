f9ec202b26545949f39065d6ff805150412aae34
ela-hidri <elahidri.eh@gmail.com>
2023-03-21 13:24:57 -0700

    adding 5_sign  file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign -  prints the sign of a number.
+ * @c: The character to check
+ *
+ * Return: if greater than zero 1.
+ * if equle to 0, 0 is returned
+ * else , -1 returnd
+ */
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if ( n == 0)
+return (0);
+else 
+return (-1);
+}
