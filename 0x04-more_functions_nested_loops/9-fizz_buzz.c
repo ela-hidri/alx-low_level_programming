75c507960b9d175f4d3b832479e0f87960d9da50
ela-hidri <elahidri.eh@gmail.com>
2023-03-23 13:54:33 -0700

    adding 9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
