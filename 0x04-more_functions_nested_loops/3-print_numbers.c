aa78f968539d2024e531e90023b84e8ce5687640
ela-hidri <elahidri.eh@gmail.com>
2023-03-23 08:45:54 -0700

    adding 3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9.
+ *
+ * Return: if true 1.
+ */
+void print_numbers(void)
+{
+int i = 48;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n')
+return (0);
+}
