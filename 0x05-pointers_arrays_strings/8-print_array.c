609b1228f0af853336cdfdf3f61fee0a572ad707
ela-hidri <elahidri.eh@gmail.com>
2023-03-27 15:41:20 -0700

    adding 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_array -  prints n elements of an array of integers
- * @a: pointer to integer 
+ * @a: pointer to integer
  * @n: n elements
  *
  * Result: nothing
@@ -10,6 +10,7 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
