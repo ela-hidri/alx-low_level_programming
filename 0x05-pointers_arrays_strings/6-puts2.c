da9ad85e14a9528f179085b04f7b705b64bb0ab3
ela-hidri <elahidri.eh@gmail.com>
2023-03-27 14:45:16 -0700

    adding 6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * @str: pointer to string
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i+=2)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');	
+}
