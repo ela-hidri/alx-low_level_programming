a3ce5a624de1254421f4d4e7fb08119b1e499b0f
ela-hidri <elahidri.eh@gmail.com>
2023-03-27 14:53:06 -0700

    adding 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,9 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i+=2)
+	for (i = 0; str[i] != '\0' && str[i + 1] != '\0'; i += 2)
 	{
 		_putchar(str[i]);
 	}
-_putchar('\n');	
+_putchar('\n');
 }
