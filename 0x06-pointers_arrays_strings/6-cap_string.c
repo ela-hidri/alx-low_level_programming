25d3dc717b510d8107eba50ceefd38bdace87879
ela-hidri <elahidri.eh@gmail.com>
2023-03-29 15:15:15 -0700

    adding 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string to capitalize
+ *
+ * Return: capitalized stirng
+ */
+char *cap_string(char *str)
+{
+char sep[] = {',',';','.','!','?','"','(',')','{','}',' ','\n','\t','\0'};
+	int i = 0;
+	int j = 1;
+
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (sep[j] != '\0')
+		{
+		if (str[i - 1] == sep[j])
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		}
+		j++;
+		}
+		i++;
+	}
+	return (str);
+}
