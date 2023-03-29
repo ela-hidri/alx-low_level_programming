7d87ed6b3a1832685dd502976d6e35bcf67ac797
ela-hidri <elahidri.eh@gmail.com>
2023-03-29 16:04:06 -0700

    adding 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * cap_string - capitalizes all words of a string.
  * @str: string to capitalize
@@ -7,30 +6,25 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
+        char sep[] = ",;.!?\"(){} \n\t";
+        int i = 0;
+        int j = 0;
 
-	while (str[i])
-	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
-		if (str[i - 1] == ' ' ||
-		    str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' ||
-		    str[i - 1] == ',' ||
-		    str[i - 1] == ';' ||
-		    str[i - 1] == '.' ||
-		    str[i - 1] == '!' ||
-		    str[i - 1] == '?' ||
-		    str[i - 1] == '"' ||
-		    str[i - 1] == '(' ||
-		    str[i - 1] == ')' ||
-		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
-		    i == 0)
-			str[i] -= 32;
-
-		i++;
-	}
-return (str);
+        while (str[i])
+        {
+                j = 0;
+                while (sep[j] != '\0')
+                {
+                        if (str[i - 1] == sep[j])
+                        {
+                                if (str[i] >= 'a' && str[i] <= 'z')
+                                {
+                                        str[i] -= 32;
+                                }
+                        }
+                        j++;
+                }
+                i++;
+        }
+        return (str);
 }
