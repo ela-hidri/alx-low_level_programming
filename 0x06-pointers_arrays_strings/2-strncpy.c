987655bdf3dd8ebc1a64830d9491675ac17321f4
ela-hidri <elahidri.eh@gmail.com>
2023-03-29 13:06:00 -0700

    adding 2-strcpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,26 @@
+#include <stdio.h>
 /**
  * _strncpy - copies a string
  * @dest: copy into
  * @src: string to be copied
- * %n: number of letters to copy
+ * @n: number of letters to copy
  *
  * Return: pinter to copied value
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *s = dest;
 	int j = 0;
 
-	while(n > 0 && *src != 0)
+	while (j < n && src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
+		dest[j] = src[j];
 		j++;
 	}
 	while (j < n)
 	{
-		*dest = '\0';
-		dest++;
+		dest[j] = '\0';
 		j++;
 	}
-	return (s);
+
+	return (dest);
 }
