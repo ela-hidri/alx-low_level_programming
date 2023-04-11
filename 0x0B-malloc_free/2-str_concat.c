ea83c90d14feea8c90be70696c1197b375691360
ela-hidri <elahidri.eh@gmail.com>
2023-04-11 15:11:26 -0700

    adding 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int l1, l2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (l1 = 0; s1[l1] != '\0'; l1++)
 		;
 	for (l2 = 0; s2[l2] != '\0'; l2++)
