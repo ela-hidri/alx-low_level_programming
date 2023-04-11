73c0ff1805271e854ca74f8e8784a52eb2f11b51
ela-hidri <elahidri.eh@gmail.com>
2023-04-11 16:05:21 -0700

    adding 3-alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **ar;
-	int i, j;
+	int i, j, k;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -25,15 +25,20 @@ int **alloc_grid(int width, int height)
 		ar[i] = malloc(width * sizeof(int));
 		if (ar[i] == NULL)
 		{
-			free(ar[i]);
+			for(k = i ; k > 0; k--)
+			{
+				free(ar[k]);
+			}
 			free(ar);
 			return (NULL);
 		}
-
+	}
+	 for (i = 0; i < height; i++)
+	 {
 		for (j = 0; j < width; j++)
 		{
 			ar[i][j] = 0;
 		}
-	}
+	 }
 	return (ar);
 }
