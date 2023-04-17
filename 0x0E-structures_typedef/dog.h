74a53a17700f0b206eb0394d623079e296b86ab8
ela-hidri <elahidri.eh@gmail.com>
2023-04-17 14:46:14 -0700

    adding dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - descripbes the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+        char * name;
+        float age;
+        char * owner;
+}
