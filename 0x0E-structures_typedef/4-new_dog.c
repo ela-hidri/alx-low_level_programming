4d7ead312b2748db0ced79bb01410c3c99803416
ela-hidri <elahidri.eh@gmail.com>
2023-04-17 16:22:41 -0700

    adding 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,25 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
+ * _strcpy - copies the string pointed to by src
+ * @dest: pointer where to copy
+ * @src: from where copy
+ *
+ * Return: copied value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
  * new_dog - declare a new dog
  * @name: name
  * @age: age
@@ -12,15 +31,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newd;
+	int ln = 0, lo = 0;
 
 	if (name == NULL || age < 0  || owner == NULL)
 		return (NULL);
 	newd = malloc(sizeof(struct dog));
 	if (newd == NULL)
 		return (NULL);
-	newd->name = name;
+	for (ln = 0; name[ln]; ln++)
+		;
+	for (lo = 0; owner[lo]; lo++)
+		;
+	newd->name = malloc(sizeof(char) * (ln + 1));
+	if (newd->name == NULL)
+	{
+		free(newd);
+		return (NULL);
+	}
+	newd->owner = malloc(sizeof(char) * (lo + 1));
+	if (newd->owner == NULL)
+	{
+		free(newd);
+		return (NULL);
+	}
+	_strcpy(newd->name, name);
 	newd->age = age;
-	newd->owner = owner;
+	_strcpy(newd->owner, owner);
 	return (newd);
 
 }
