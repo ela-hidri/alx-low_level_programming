8d889e12b68bc442d8e1a9e5f21dcea4df787fad
ela-hidri <elahidri.eh@gmail.com>
2023-04-19 15:08:39 -0700

    adding 3-op_functions.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - performs simple operations.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Success(always)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int a, b;
+	char *o;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	o = argv[2];
+	b = atoi(argv[3]);
+
+	if ((*o == '/' || *o == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (get_op_func(o) == NULL || *(o + 1) != '\0')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", get_op_func(o)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - calculate sum of a and b
+ * @a: arg
+ * @b: arg
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - calculate sum of a and b
+ * @a: arg
+ * @b: arg
+ *
+ * Return:  difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: arg
+ * @b: arg
+ *
+ * Return:  multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of a by b
+ * @a: arg
+ * @b: arg
+ *
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder of the division
+ * @a: arg
+ * @b: arg
+ *
+ * Return: remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
