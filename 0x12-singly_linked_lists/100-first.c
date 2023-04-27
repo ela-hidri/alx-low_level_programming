aa239c624eeb312d814a9a4f8e0b64661bc9c44e
ela-hidri <elahidri.eh@gmail.com>
2023-04-27 14:28:54 -0700

    adding 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-void before_main(void) __attribute__((constructor));
-void before_main(void)
+/**
+ * print_before_main - prints before main
+ * Return: nothing
+ */
+void print_before_main(void) __attribute__((constructor));
+void print_before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
