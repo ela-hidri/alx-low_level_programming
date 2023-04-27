61917e3b8c601f7ac06e647a192eb987533ae802
ela-hidri <elahidri.eh@gmail.com>
2023-04-27 14:02:37 -0700

    fixing 3-main.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,14 +29,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *tail;
 
 	new = malloc(sizeof(list_t));
-	new->str = strdup(str);
-	new->len = _strlen(str);
-	new->next = NULL;
 	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
