14e82405d1e2b57f52e6b70387103e1e174305d0
ela-hidri <elahidri.eh@gmail.com>
2023-05-01 13:10:41 -0700

    adding 8-sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: node head
+ *
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *tmp = head;
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
