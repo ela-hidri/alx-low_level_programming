91e7f2535a168b683412163665382f57594ab8fe
ela-hidri <elahidri.eh@gmail.com>
2023-05-08 15:45:12 -0700

    adding 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	for (l = 0; text_content[l]; l++)
-		;
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l]; l++)
+			;
+	}
 	fo = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fo < 0)
 		return (-1);
