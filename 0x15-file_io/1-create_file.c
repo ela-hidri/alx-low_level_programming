2339eeec3bd23bc18fdabbe4336e16df97820d93
ela-hidri <elahidri.eh@gmail.com>
2023-05-08 15:28:27 -0700

    adding 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * create_file -  creates a file.
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fo, fw, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+	for (l = 0; text_content[l]; l++)
+		;
+	fo = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fo < 0)
+		return (-1);
+	fw = write(fo, text_content, l);
+	if (fw < 0)
+		return (-1);
+	close(fo);
+	return (1);
+}
