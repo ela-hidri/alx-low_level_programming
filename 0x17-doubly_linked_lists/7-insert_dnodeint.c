88e0da1d19eb1347b162d6a786bd3e329a77e904
ela-hidri <elahidri.eh@gmail.com>
2023-06-15 14:21:39 -0700

    adding 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list.
- * @h: list
- *
- * Return: number of elements
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	dlistint_t *tmp;
-	size_t l = 0;
-
-	tmp = (dlistint_t *)h;
-	while (tmp)
-	{
-		tmp = tmp->next;
-		l++;
-	}
-	return (l);
-}
-/**
  * insert_dnodeint_at_index -  inserts a new node at a given position.
  * @h: list
  * @idx: index
@@ -30,16 +11,10 @@ size_t dlistint_len(const dlistint_t *h)
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp, *new;
-	unsigned int i = 0, l;
+	unsigned int i = 0;
 
-	l = (unsigned int)dlistint_len(*h);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == (l + 1))
-		return (add_dnodeint_end(h, n));
-	if (idx > (l + 1))
-		return (NULL);
-
 	tmp = *h;
 	new = malloc(sizeof(dlistint_t));
 	if (new  == NULL)
@@ -48,6 +23,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (idx == i + 1)
 		{
+			if (tmp->next == NULL)
+				return (add_dnodeint_end(h, n));
 			new->n = n;
 			new->prev = tmp;
 			new->next = tmp->next;
