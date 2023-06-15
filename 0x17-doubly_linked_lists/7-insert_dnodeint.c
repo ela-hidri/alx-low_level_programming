3abf220673c58744b92f954fcddad59ebe36c659
ela-hidri <elahidri.eh@gmail.com>
2023-06-15 14:34:00 -0700

    fixing 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = tmp;
 			new->next = tmp->next;
 			tmp->next = new;
+			tmp->next->prev = new;
 			return (new);
 		}
 		i++;
