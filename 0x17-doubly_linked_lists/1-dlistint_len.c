5ece9d4aeb96e766866047128b6a03a8fb914668
ela-hidri <elahidri.eh@gmail.com>
2023-06-15 12:19:55 -0700

    adding 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: list
+ *
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *tmp;
+	size_t l = 0;
+
+	tmp = (dlistint_t *)h;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		l++;
+	}
+	return (l);
+}
