cd2d013bde4efb01a3d77fac4a330a87358abe95
ela-hidri <elahidri.eh@gmail.com>
2023-07-07 16:11:26 -0700

    fixing issued

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,11 +14,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index > ht->size)
+		return (NULL);
 	node = ht->array[index];
-	if (node != NULL)
+	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
+		node = node->next;
 	}
 	return (NULL);
 
