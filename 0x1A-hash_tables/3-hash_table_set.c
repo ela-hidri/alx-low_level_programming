3f6a27fba17f2cae4a6300671fd12554195be39c
ela-hidri <elahidri.eh@gmail.com>
2023-07-07 16:17:21 -0700

    fixing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,8 +19,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	cp_value = strdup(value);
-        if (cp_value == NULL)
-                return (0);
+	if (cp_value == NULL)
+		return (0);
 
 	for (i = index; ht->array[i]; i++)
 	{
