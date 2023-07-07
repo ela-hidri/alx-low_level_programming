02287e21c06f4949347b2c5c0080423a974ad50d
ela-hidri <elahidri.eh@gmail.com>
2023-07-07 15:03:41 -0700

    adding 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (m > 0)
 					printf(", ");
-				printf("'%s: '%s'", node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
 				m++;
 			}
